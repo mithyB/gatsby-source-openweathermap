3982e25b0e4391f77f0b101a1d6bdbd53ea4b7ba
Michael Buholzer <michael.buholzer@outlook.com>
2020-09-24 20:25:38 +0200

    update api

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,14 +1,13 @@
 import "@babel/polyfill";
 
-const fetch = require('node-fetch');
-const crypto = require('crypto');
+const fetch = require("node-fetch");
+const crypto = require("crypto");
 
 exports.sourceNodes = async (
   { actions: { createNode }, createNodeId },
   { plugins, ...options }
 ) => {
-
-  const apiUrl = `https://api.openweathermap.org/data/2.5/${options.type}?q=${options.location}&units=${options.units}&apikey=${options.apikey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/${options.type}?q=${options.location}&units=${options.units}&appid=${options.apikey}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
 
@@ -18,12 +17,12 @@ exports.sourceNodes = async (
     parent: null,
     children: [],
     internal: {
-      type: 'OpenWeather',
+      type: "OpenWeather",
       content: JSON.stringify(data),
       contentDigest: crypto
-        .createHash('md5')
+        .createHash("md5")
         .update(JSON.stringify(data))
-        .digest('hex'),
+        .digest("hex"),
     },
   });
 };
