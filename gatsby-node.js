8cbe3deb044ade1412cdda42fc1b8394369c6e34
Michael Buholzer <michael.buholzer@outlook.com>
2020-09-24 20:27:08 +0200

    changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -118,9 +118,9 @@ require("core-js/modules/es6.weak-set");
 
 require("regenerator-runtime/runtime");
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { keys.push.apply(keys, Object.getOwnPropertySymbols(object)); } if (enumerableOnly) keys = keys.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); return keys; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -132,16 +132,12 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-var fetch = require('node-fetch');
+var fetch = require("node-fetch");
 
-var crypto = require('crypto');
+var crypto = require("crypto");
 
-exports.sourceNodes =
-/*#__PURE__*/
-function () {
-  var _ref3 = _asyncToGenerator(
-  /*#__PURE__*/
-  regeneratorRuntime.mark(function _callee(_ref, _ref2) {
+exports.sourceNodes = /*#__PURE__*/function () {
+  var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(_ref, _ref2) {
     var createNode, createNodeId, plugins, options, apiUrl, response, data;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
@@ -149,7 +145,7 @@ function () {
           case 0:
             createNode = _ref.actions.createNode, createNodeId = _ref.createNodeId;
             plugins = _ref2.plugins, options = _objectWithoutProperties(_ref2, ["plugins"]);
-            apiUrl = `https://api.openweathermap.org/data/2.5/${options.type}?q=${options.location}&units=${options.units}&apikey=${options.apikey}`;
+            apiUrl = `https://api.openweathermap.org/data/2.5/${options.type}?q=${options.location}&units=${options.units}&appid=${options.apikey}`;
             _context.next = 5;
             return fetch(apiUrl);
 
@@ -160,14 +156,14 @@ function () {
 
           case 8:
             data = _context.sent;
-            createNode(_objectSpread({}, data, {
+            createNode(_objectSpread(_objectSpread({}, data), {}, {
               id: createNodeId(`weather-${data.id}`),
               parent: null,
               children: [],
               internal: {
-                type: 'OpenWeather',
+                type: "OpenWeather",
                 content: JSON.stringify(data),
-                contentDigest: crypto.createHash('md5').update(JSON.stringify(data)).digest('hex')
+                contentDigest: crypto.createHash("md5").update(JSON.stringify(data)).digest("hex")
               }
             }));
 
